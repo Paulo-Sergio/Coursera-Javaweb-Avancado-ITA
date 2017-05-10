279d8eae04a7a4bb5e08e721a27e974bd5aa75ce
Paulo-Sergio <paulofranca.contato@gmail.com>
2017-05-10 10:52:23 -0300

    tarefa final da semana 3

--- semana3/src/tarefafinal/teste/TesteUsuarioDAO.java
+++ semana3/src/tarefafinal/teste/TesteUsuarioDAO.java
@@ -1,4 +1,4 @@
-package tarefa1.acessobanco.teste;
+package tarefafinal.teste;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,8 +13,8 @@ import org.dbunit.util.fileloader.FlatXmlDataFileLoader;
 import org.junit.Before;
 import org.junit.Test;
 
-import tarefa1.acessobanco.dao.UsuarioDAO;
-import tarefa1.acessobanco.model.UsuarioTeste;
+import tarefafinal.dao.UsuarioDAO;
+import tarefafinal.model.Usuario;
 
 public class TesteUsuarioDAO {
 
@@ -25,32 +25,46 @@ public class TesteUsuarioDAO {
 		this.jdt = new JdbcDatabaseTester("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/coursera_usuarios",
 				"root", "");
 		FlatXmlDataFileLoader loader = new FlatXmlDataFileLoader();
-		this.jdt.setDataSet(loader.load("/tarefa1/acessobanco/teste/inicio.xml"));
+		this.jdt.setDataSet(loader.load("/tarefafinal/teste/inicio.xml"));
 		this.jdt.onSetup();
 	}
 
 	@Test
-	public void recuperaTodos() {
-		List<UsuarioTeste> lista = UsuarioDAO.todosUsuarios();
+	public void testeRanking() {
+		List<Usuario> lista = new UsuarioDAO().ranking();
 		assertEquals(2, lista.size());
 		assertEquals("joao", lista.get(0).getLogin());
 	}
-	
+
 	@Test
-	public void insereNovo() throws SQLException, Exception {
-		UsuarioTeste u = new UsuarioTeste();
-		u.setLogin("duda");
-		u.setNome("Maria Eduarda");
-		u.setEmail("duda@gmail.com");
-		
-		UsuarioDAO.inserirUsuario(u);
-		
-		IDataSet currentDataSet = this.jdt.getConnection().createDataSet();
-		ITable currentTable = currentDataSet.getTable("USUARIO_TESTE");
-		FlatXmlDataFileLoader loader = new FlatXmlDataFileLoader();
-		IDataSet expectedDataSet = loader.load("/tarefa1/acessobanco/teste/verifica.xml");
-		ITable expectedTable = expectedDataSet.getTable("USUARIO_TESTE");
-		Assertion.assertEquals(expectedTable, currentTable);
+	public void testeRecuperar() {
+		Usuario u = new UsuarioDAO().recuperar("teste");
+		assertEquals("sobre@testando.com.br", u.getEmail());
 	}
 
+	@Test
+	public void testeAdicionarPontos() throws SQLException, Exception {
+		new UsuarioDAO().adicionarPontos("joao", 5);
+		Usuario u = new UsuarioDAO().recuperar("joao");
+		assertEquals(15, (int) u.getPontos());
+	}
+
+	@Test
+	public void testeInserir() throws SQLException, Exception {
+		Usuario u = new Usuario();
+		u.setLogin("duda");
+		u.setEmail("duda@gmail.com");
+		u.setNome("Maria Eduarda");
+		u.setSenha("duda123");
+		u.setPontos(15);
+
+		new UsuarioDAO().inserir(u);
+
+		IDataSet currentDataSet = this.jdt.getConnection().createDataSet();
+		ITable currentTable = currentDataSet.getTable("USUARIO");
+		FlatXmlDataFileLoader loader = new FlatXmlDataFileLoader();
+		IDataSet expectedDataSet = loader.load("/tarefafinal/teste/verifica.xml");
+		ITable expectedTable = expectedDataSet.getTable("USUARIO");
+		Assertion.assertEquals(expectedTable, currentTable);
+	}
 }
